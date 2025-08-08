767b660e0cf5cde96621fe2ed4a6dbca1953bbc3
mohamedhani2233 <vaimohamedhani@gmail.com>
2025-08-08 21:10:18 +0200

    Create task 3.secssion4.cpp

---
+++ task 3.secssion4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	do {
+		cout << "entre the number greater than10;";
+		cin >> num;
+	} while (num <= 10); {
+		cout << "you entered an nviald number" << num << endl;
+	}
+	}
